af79352f0a70a095c6c6f6db45b906ca7f3003b5
Leo Cheung <leo.cheung@cbsinteractive.com>
2022-09-08 11:14:26 -0400

    fetch movies on SSR

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,11 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
 import Results from '../components/Results'
+import requests from '../utils/requests'
 
-export default function Home() {
+export default function Home({results}) {
+  // console.log(results);
+  
   return (
     <div>
       <Head>
@@ -14,7 +17,22 @@ export default function Home() {
 
       <Header />
       <Nav />
-      <Results />
+      <Results results={results} />
     </div>
   )
+}
+
+// fetch movie data from api
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+  // console.log(genre);
+
+  const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`).then(res => res.json());
+  // console.log(request);
+
+  return {
+    props: {
+      results: request.results
+    }
+  }
 }
