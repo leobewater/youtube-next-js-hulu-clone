0cf0dcaece8f6923fb5183c7e6bddf84d0cd537c
Leo Cheung <leo.cheung@cbsinteractive.com>
2022-09-08 10:03:54 -0400

    finish header and header items styling

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,19 @@
-import Image from "next/image"
 import HeaderItem from "./HeaderItem"
-import { HomeIcon, CheckBadgeIcon } from '@heroicons/react/24/outline'
+import { HomeIcon, BoltIcon, CheckBadgeIcon, RectangleStackIcon, MagnifyingGlassIcon, UserIcon } from '@heroicons/react/24/outline'
 
 function Header() {
     return (
-        <header className="">
-            <div className="">
-                <HeaderItem title="Home" Icon={HomeIcon} />
-                <HeaderItem title="Badge" Icon={CheckBadgeIcon} />
+        <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+            <div className="flex flex-grow justify-evenly max-w-2xl">
+                <HeaderItem title="HOME" Icon={HomeIcon} />
+                <HeaderItem title="TRENDING" Icon={BoltIcon} />
+                <HeaderItem title="VERIFIED" Icon={CheckBadgeIcon} />
+                <HeaderItem title="COLLECTIONS" Icon={RectangleStackIcon} />
+                <HeaderItem title="SEARCH" Icon={MagnifyingGlassIcon} />
+                <HeaderItem title="ACCOUNT" Icon={UserIcon} />
             </div>
-            <div className="">
-                <svg viewBox="0 -2 61 24" height="24" width="61" role="img" aria-labelledby="logoTitle" fill="rgb(255, 255, 255)"><title id="logoTitle">hulu</title><path d="M9.57046613 6.24096927h-3.1015504c-1.0875017 0-1.63435327.29263768-1.63435327.29263768V0H0v20h4.83456246v-8.0704467c0-.7137436.58067743-1.29171 1.2958127-1.29171h2.81374862c.7162628 0 1.29524892.5779664 1.29524892 1.29171V20h4.8359719v-8.6950777c0-3.65670596-2.4444829-5.06395303-5.50487847-5.06395303zm46.48012787 0v8.06932223c0 .714868-.579268 1.2931156-1.294967 1.2931156h-2.8134668c-.7162628 0-1.2952489-.5782476-1.2952489-1.2931156V6.24096927h-4.8368175V14.712844c0 3.4627386 2.2220777 5.287156 5.5062878 5.287156h4.7347762v-.0247379c3.0245965 0 4.8354081-2.1465718 4.8354081-5.2624181V6.24096927H56.050594zM28.7728487 14.3102915c0 .714868-.5798318 1.2931156-1.2955308 1.2931156h-2.8137486c-.7162628 0-1.2955308-.5782476-1.2955308-1.2931156V6.24096927h-4.8359719V14.712844c0 3.4627386 2.2220778 5.287156 5.506006 5.287156h4.7347761v-.0247379c3.0248785 0 4.8359719-2.1465718 4.8359719-5.2624181V6.24096927h-4.8359719v8.06932223zM37.294431 20h4.8365357V0H37.294431v20z" fill-rule="evenodd"></path></svg>
-                {/* <Image className="object-contain" src="https://links.parareact.com/ua6" width={200} height={100} alt="Logo" /> */}
+            <div>
+                <svg viewBox="0 -2 61 24" height="30" width="81" role="img" aria-labelledby="logoTitle" fill="rgb(255, 255, 255)"><title id="logoTitle">hulu</title><path d="M9.57046613 6.24096927h-3.1015504c-1.0875017 0-1.63435327.29263768-1.63435327.29263768V0H0v20h4.83456246v-8.0704467c0-.7137436.58067743-1.29171 1.2958127-1.29171h2.81374862c.7162628 0 1.29524892.5779664 1.29524892 1.29171V20h4.8359719v-8.6950777c0-3.65670596-2.4444829-5.06395303-5.50487847-5.06395303zm46.48012787 0v8.06932223c0 .714868-.579268 1.2931156-1.294967 1.2931156h-2.8134668c-.7162628 0-1.2952489-.5782476-1.2952489-1.2931156V6.24096927h-4.8368175V14.712844c0 3.4627386 2.2220777 5.287156 5.5062878 5.287156h4.7347762v-.0247379c3.0245965 0 4.8354081-2.1465718 4.8354081-5.2624181V6.24096927H56.050594zM28.7728487 14.3102915c0 .714868-.5798318 1.2931156-1.2955308 1.2931156h-2.8137486c-.7162628 0-1.2955308-.5782476-1.2955308-1.2931156V6.24096927h-4.8359719V14.712844c0 3.4627386 2.2220778 5.287156 5.506006 5.287156h4.7347761v-.0247379c3.0248785 0 4.8359719-2.1465718 4.8359719-5.2624181V6.24096927h-4.8359719v8.06932223zM37.294431 20h4.8365357V0H37.294431v20z" fill-rule="evenodd"></path></svg>
             </div>
         </header>
     )
